dc34da8d67a4a3822199674a23b4843da2dc0be9
tlhol <tlhol@TLHOLO>
2026-08-08 14:56:13 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Statistics_and_Testing
+{
+    public partial class frmLibrary : Form
+    {
+
+        // These lists will store playlist data
+        private List<string> songNames = new List<string>();
+        private List<string> artists = new List<string>();
+        private List<TimeSpan> durations = new List<TimeSpan>();
+
+        public frmLibrary()
+        {
+            InitializeComponent();
+        }
+
+        private void myWrappedToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmStats statsForm = new frmStats(this);
+            statsForm.Show();
+            this.Hide();
+        }
+
+        private void UpdateStatistics()
+        {
+            // STATISTIC 1: Total number of tracks
+            int totalTracks = songNames.Count;
+            lblTotalSongs.Text = "Total Songs: " + totalTracks.ToString();
+
+            // STATISTIC 2: Total duration
+            TimeSpan totalDuration = TimeSpan.Zero;
+            foreach (TimeSpan duration in durations)
+            {
+                totalDuration = totalDuration + duration;
+            }
+
+            // Format as Hours:Minutes:Seconds
+            lblDuration.Text = "Total Duration: " + totalDuration.ToString(@"hh\:mm\:ss");
+
+            // STATISTIC 3: Most common artist
+            if (artists.Count > 0)
+            {
+                var artistCounts = new Dictionary<string, int>();
+                foreach (string artist in artists)
+                {
+                    if (artistCounts.ContainsKey(artist))
+                        artistCounts[artist]++;
+                    else
+                        artistCounts[artist] = 1;
+                }
+
+                string mostCommon = "N/A";
+                int maxCount = 0;
+                foreach (var kvp in artistCounts)
+                {
+                    if (kvp.Value > maxCount)
+                    {
+                        maxCount = kvp.Value;
+                        mostCommon = kvp.Key;
+                    }
+                }
+            }
+        }
+
+        //Store data to pass it to frmStats
+        public List<string> GetSongNames()
+        {
+            return songNames;
+        }
+
+        public List<TimeSpan> GetDurations()
+        {
+            return durations;
+        }
+
+        public int GetTotalTracks()
+        {
+            return songNames.Count;
+        }
+
+        public TimeSpan GetTotalDuration()
+        {
+            TimeSpan total = TimeSpan.Zero;
+            foreach (TimeSpan duration in durations)
+            {
+                total = total + duration;
+            }
+            return total;
+        }
+    }
+}
+
+
+
---
+++ frmStats.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Statistics_and_Testing
+{
+    public partial class frmStats : Form
+    {
+        // Store reference to the library form
+        private frmLibrary libraryForm;
+
+        public frmStats(frmLibrary library)
+        {
+            InitializeComponent();
+            libraryForm = library;
+
+            // Display statistics when form loads
+            DisplayStatistics();
+        }
+
+        private void DisplayStatistics()
+        {
+            // Get data from the library form
+            int totalTracks = libraryForm.GetTotalTracks();
+            TimeSpan totalDuration = libraryForm.GetTotalDuration();
+
+            // Update the labels
+            lblWrappedNumber.Text = totalTracks.ToString();
+            lblWrappedStatement.Text = "Total Tracks in Your Library";
+
+            // Find and update the duration label
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is Label && ctrl.Name == "lblDurationDisplay")
+                {
+                    ctrl.Text = "Total Duration: " + totalDuration.ToString(@"hh\:mm\:ss");
+                }
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            libraryForm.Show();
+            this.Close();
+        }
+    }
+}
