908760ba8e1459210111f26bb663620f0c3070ec
TerciaMosh <terciamosh@gmail.com>
2026-08-10 21:41:50 +0200

    added try/catch

--- Practical_1assignment/Form1.cs
+++ Practical_1assignment/Form1.cs
@@ -20,38 +20,47 @@ namespace Assignment_1
 
         private void btnLOGIN_Click(object sender, EventArgs e)
         {
-            string username = txtUsername.Text;
-            string password = txtPassword.Text;
-
-            if (txtPassword.Text == "" || txtUsername.Text == "")
+            try
             {
-                MessageBox.Show("Please enter both your username and password");
-                return;
+                string username = txtUsername.Text;
+                string password = txtPassword.Text;
+
+                if (txtPassword.Text == "" || txtUsername.Text == "")
+                {
+                    MessageBox.Show("Please enter both your username and password");
+                    return;
+                }
+
+                StreamReader contentFile;
+                contentFile = new StreamReader("File with stuff.txt");
+                string lines = contentFile.ReadLine();
+                bool loginSuccessful = false;
+
+                while (lines != null)
+                {
+                    string[] userDetails = lines.Split(',');
+                    string savedusername = userDetails[0];
+                    string savedpassword = userDetails[1];
+                }
+                lines = contentFile.ReadLine();
+
+                if (loginSuccessful)
+                {
+                    frmHomePage homepage = new frmHomePage();
+                    homepage.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Your details do not exist. Please try again or register a new account");
+                }
             }
-
-            StreamReader contentFile;
-            contentFile = new StreamReader("File with stuff.txt");
-            string lines = contentFile.ReadLine();
-            bool loginSuccessful = false;
-
-            while (lines != null)
+            catch (Exception ex)
             {
-                string[] userDetails = lines.Split(',');
-                string savedusername = userDetails[0];
-                string savedpassword = userDetails[1];
-            }
-            lines = contentFile.ReadLine();
-
-            if (loginSuccessful)
-            {
-                frmHomePage homepage = new frmHomePage();
-                homepage.Show();
-                this.Hide();
-            }
-            else
-            {
-                MessageBox.Show("Your details do not exist. Please try again or register a new account");
+                MessageBox.Show("An error occurred: " + ex.Message);
             }
         }
     }
 }
+    
+
