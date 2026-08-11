4f9c4a02e5389ff0b61659f2764cdc98d95b76ad
Karabo <u26688124@tuks.co.za>
2026-08-11 20:11:31 +0200

    Resolve stash conflicts and update main project

--- Practical_1assignment/Program.cs
+++ Practical_1assignment/Program.cs
@@ -16,7 +16,7 @@ namespace Practical_1assignment
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmHomePage("Jack"));
         }
     }
 }
