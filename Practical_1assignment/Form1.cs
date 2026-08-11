71407a827eb5e3a56ee21c777ea7efb9423b5aca
Reba-Mothupi <rebamothupi2@gmail.com>
2026-08-11 19:37:28 +0200

    Fixed building errors

--- Practical_1assignment/Form1.cs
+++ Practical_1assignment/Form1.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace Deliverable_1b
 {
@@ -18,7 +18,7 @@ namespace Deliverable_1b
         public Registerfrm()
         {
             InitializeComponent();
-            InitializeFile();
+            
         }
 
         private void Registerfrm_Load(object sender, EventArgs e)
