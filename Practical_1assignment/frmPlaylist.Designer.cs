e24b9889735c28c327f6cbeff2663ce5b3e59c14
InfinityWar <savuzwampayipheli590@gmail.com>
2026-08-11 14:48:57 +0200

    Deliverable 1 UI for Playlist Form completed

---
+++ Practical_1assignment/frmPlaylist.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Practical_1assignment
+{
+    partial class frmPlaylist
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.pbCover = new System.Windows.Forms.PictureBox();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCover)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(199, 37);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(27, 13);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Title";
+            this.lblTitle.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(447, 37);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(30, 13);
+            this.lblDate.TabIndex = 1;
+            this.lblDate.Text = "Date";
+            this.lblDate.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pbCover
+            // 
+            this.pbCover.Location = new System.Drawing.Point(262, 69);
+            this.pbCover.Name = "pbCover";
+            this.pbCover.Size = new System.Drawing.Size(155, 155);
+            this.pbCover.TabIndex = 2;
+            this.pbCover.TabStop = false;
+            this.pbCover.Click += new System.EventHandler(this.pbCover_Click);
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(286, 249);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(105, 23);
+            this.btnUpload.TabIndex = 3;
+            this.btnUpload.Text = "Upload Art Cover";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(53, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(8, 8);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // frmPlaylist
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.pbCover);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "frmPlaylist";
+            this.Text = "frmPlaylist";
+            this.Load += new System.EventHandler(this.frmPlaylist_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCover)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.PictureBox pbCover;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Practical_1assignment/frmPlaylist.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practical_1assignment
+{
+    public partial class frmPlaylist : Form
+    {
+        // 1. Variable to hold the title passed from Home
+        private string playlistTitle;
+        public frmPlaylist(string title)
+        {
+            InitializeComponent();
+            // Catching the title 
+            playlistTitle = title;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmPlaylist_Load(object sender, EventArgs e)
+        {
+            // Show the title passed from the Home form
+            lblTitle.Text = "Playlist: " + playlistTitle;
+
+            // Generate today's date directly from the system
+            lblDate.Text = "Created: " + DateTime.Now.ToShortDateString();
+        }
+
+        private void pbCover_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            // Create a new dialog box to let the user browse their computer for a file
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            // Open the dialog and check if the user successfully selected a file and clicked "OK"
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Set the PictureBox image path to the exact file the user just selected
+                    pbCover.ImageLocation = openFileDialog.FileName;
+                    // Adjust the image size so it stretches to fit perfectly inside the PictureBox
+                    pbCover.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                catch (Exception ex)
+                {
+                    // If an error occurs (like selecting a corrupted file), show an error message instead of crashing
+                    MessageBox.Show("Error loading image: " + ex.Message);
+                }
+            }
+        }
+    }
+}
